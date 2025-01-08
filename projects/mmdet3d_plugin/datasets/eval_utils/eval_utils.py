592daf11758912fc7e6e7a315cd00be467c8176e
hamadayo <hama.hocoroco@outlook.jp>
2025-01-08 18:13:59 +0900

    計画中止の残骸

--- projects/mmdet3d_plugin/datasets/eval_utils/eval_utils.py
+++ projects/mmdet3d_plugin/datasets/eval_utils/eval_utils.py
@@ -460,12 +460,12 @@ def load_gt(nusc: NuScenes, eval_split: str, box_cls, verbose: bool = False, cat
         scene_token = nusc.get('sample', sample_token)['scene_token']
         scene_record = nusc.get('scene', scene_token)
         # !!!!!!!ここを変更
-        if scene_record['name'] in splits[eval_split]:
+        # if scene_record['name'] in splits[eval_split]:
+        #     sample_tokens.append(sample_token)
+        if nusc.version.endswith('mini'):
             sample_tokens.append(sample_token)
-        # if nusc.version.endswith('mini'):
-        #     sample_tokens.append(sample_token)
-        # elif scene_record['name'] in splits[eval_split]:
-        #     sample_tokens.append(sample_token)
+        elif scene_record['name'] in splits[eval_split]:
+            sample_tokens.append(sample_token)
 
     all_annotations = EvalBoxes()
 
